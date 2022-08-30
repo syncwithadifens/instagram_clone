fe08bac087251c17ff2992f3291b088e8505d8d7
Afiv Dicky Efendy <dickyareda@gmail.com>
2022-08-30 21:03:56 +0700

    adding top bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: Column(
-        children: [
-          Row(
-            children: [
-              Image.asset('assets/img/insta_logo.png'),
-            ],
-          )
-        ],
-      )),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness: Brightness.dark),
+      child: Scaffold(
+        body: SafeArea(
+            child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15),
+                    child: Image.asset('assets/img/insta_logo.png'),
+                  ),
+                  const Spacer(),
+                  Image.asset('assets/img/Add_post.png'),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 15, left: 20),
+                    child: Image.asset('assets/img/DM.png'),
+                  ),
+                ],
+              ),
+            )
+          ],
+        )),
+      ),
     );
   }
 }
